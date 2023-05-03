f23d84865f8292d195ddc1a0f96e1b12e4a58686
rachid ouhammou <rachidouhammou21@gmail.com>
2023-05-03 10:50:43 +0100

    finishing upload and download functionality.

--- website/urls.py
+++ website/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('result/', views.result, name="result"),
     path('upload-cv/', views.upload, name="upload-cv"),
+    path('download/<path:file_path>/', views.download_file, name='download_file'),
 ]
